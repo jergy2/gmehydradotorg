1de8975591ee181f78f96f22fc68b72852e1c9cd
gmehydra <gmehydra@proton.me>
2022-12-28 11:43:35 -0800

    changed url in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888',
+    baseUrl: 'http://www.gmehydra.org',
     supportFile: false,
   },
 });
