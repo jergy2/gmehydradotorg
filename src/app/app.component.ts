5595d95dfb655894b4643c5690d501a3f19a5dfc
gmehydra <gmehydra@proton.me>
2022-12-28 09:57:41 -0800

    First change, starting fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'GME Hydra';
 }
